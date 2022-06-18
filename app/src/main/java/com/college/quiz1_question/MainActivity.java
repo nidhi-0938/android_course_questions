d95630eb94c74c0607d4a34b0c5ec9cd83110625
Adewole <wolejr@gmail.com>
2022-06-17 22:19:11 -0400

    Practice 2 update

--- app/src/main/java/com/college/quiz1_question/MainActivity.java
+++ app/src/main/java/com/college/quiz1_question/MainActivity.java
@@ -3,32 +3,89 @@ package com.college.quiz1_question;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    int mCounter;
+    private final String LOG_TAG="MainActivity";
+    // EditText view for the website URI
+    private EditText mWebsiteEditText;
+    // EditText view for the location URI
+    private EditText mLocationEditText;
+    // EditText view for the share text
+    private EditText mShareTextEditText;
+    //EditText for Dial action
+    private EditText mDialEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // This EditText contains the user input
-        // for the number of burgers
-        EditText numberStr = findViewById(R.id.numberId);
-
-        // This textview displays the result of multiplying
-        // the content of the numberId EditText by 5
-        TextView resultTv = findViewById(R.id.resultId);
-
-        // TODO
-        // Implement the 2 listeners for buttons 'clear' and 'pay'
-        // One burger is $5
-        // Result should contain 5 multiplied by number chosen
-        // Display should be prefixed with the '$' sign such as $15
-        // Reset should clear all the fields
+        mWebsiteEditText = findViewById(R.id.website_edittext);
+        mLocationEditText = findViewById(R.id.location_editext);
+        mShareTextEditText = findViewById(R.id.share_edittext);
+        mDialEditText = findViewById(R.id.phone_edittext);
 
     }
+
+    // TODO
+    /**
+     * Handles the onClick for the "Open Website" button.  Gets the URI
+     * from the edit text and sends an implicit intent for that URL.
+     *
+     * @param view The view (Button) that was clicked.
+     */
+    public void openWebsite(View view) {
+    }
+
+    // TODO
+    /**
+     * Handles the onClick for the "Open Location" button.  Gets the location
+     * text from the edit text and sends an implicit intent for that location.
+     *
+     * The location text can be any searchable geographic location.
+     *
+     * @param view The view (Button) that was clicked.
+     */
+    public void openLocation(View view) {
+
+    }
+
+    // TODO
+    /**
+     * Handles the onClick for the "Share This Text" button.  The
+     * implicit intent here is created by the  {@link ShareCompat.IntentBuilder}
+     * class.  An app chooser appears with the available options for sharing.
+     *
+     * ShareCompat.IntentBuilder is from the v4 Support Library.
+     *
+     * @param view The view (Button) that was clicked.
+     */
+    public void shareText(View view) {
+
+    }
+
+    // TODO
+    /**
+     * Handles the onClick for the "Dial" button.
+     *
+     * @param view The view (Button) that was clicked.
+     */
+    public void dial(View view) {
+    }
+
+    // TODO
+    /**
+     * Handles the onClick for the "View Contacts" button.
+     *
+     * @param view The view (Button) that was clicked.
+     */
+    public void viewContact(View view) {
+
+    }
+
 }
